1e2ecbdaf5e8b8f5de65d4453c47daa1f30afba1
form_app <subbublockchaindoveloper@gmail.com>
2023-10-04 11:15:04 +0530

    changes done Album to users.h

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ Future<users> fetchusers() async {
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    throw Exception('Failed to load album');
+    throw Exception('Failed to load Users');
   }
 }
 
